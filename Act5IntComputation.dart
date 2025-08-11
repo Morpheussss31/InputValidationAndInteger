754b44b95e78680abb5bf64ab127c4e65245d94c
John Rey <168385617+Morpheussss31@users.noreply.github.com>
2025-08-11 02:28:10 +0000

    User Input Validation and Integer Computation

--- Act5IntComputation.dart
+++ Act5IntComputation.dart
@@ -1,24 +1,25 @@
-Activity 2
+import 'dart:io';
+
 void main() {
   print("==Simple Calculator==");
 
   print("Enter the 1st number: ");
-  double? number1 = double.tryParse(stdin.readLineSync()!);
 
+  String? input1 = stdin.readLineSync();
+  double? number1 = double.tryParse(input1 ?? '');
 
   print("Enter an operator (+, -, *, /): ");
   String? operator = stdin.readLineSync();
 
-
   print("Enter the 2nd number: ");
-  double? number2 = double.tryParse(stdin.readLineSync()!);
+  String? input2 = stdin.readLineSync();
+  double? number2 = double.tryParse(input2 ?? '');
 
- 
-  if (number1 == null || number2 == null || operator == null) {
-    print("Invalid Please enter valid numbers and operator.");
+
+  if (number1 == null || number2 == null || operator == null || operator.isEmpty) {
+    print("Invalid input. Please ensure valid numbers and an operator are provided.");
     return;
   }
-
 
   double? result;
   switch (operator) {
@@ -35,13 +36,15 @@ void main() {
       if (number2 != 0) {
         result = number1 / number2;
       } else {
-        print("Error:Zero is not allow in division.");
-        return;
+        print("Error: Cannot divide by zero.");
+        return; 
       }
       break;
     default:
       print("Invalid operator. Please use +, -, *, or /.");
-      return;
+      return; 
   }
+
+  
   print("Result: $number1 $operator $number2 = $result");
 }
